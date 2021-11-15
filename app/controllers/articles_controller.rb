72eb364f6b7cc48795f4bcdae544854727573a2e
Ganeshg01 <ganesh.gajakosh@amagi.com>
2021-11-16 01:56:01 +0530

    added views to all functionality

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,7 @@
 class ArticlesController < ApplicationController
+    before_action :set_article, only: [:show, :edit, :update,:destroy]
     def show
         # byebug
-        @article  = Article.find(params[:id])
 
     end
 
@@ -9,5 +9,46 @@ class ArticlesController < ApplicationController
         @articles = Article.all
     end
 
+    def new
+        @article = Article.new
+    end
 
+    def edit
+        
+    end
+
+    def create
+        @article = Article.new(article_params)
+        if @article.save
+          flash[:notice] = "Article was created succesfully"
+          redirect_to @article  #article_path  is same as( @article)
+        else
+            render 'new'
+        end
+    end
+
+    def update
+        if @article.update(article_params)
+            flash[:notice] = "Article updated succesfully"
+            redirect_to @article
+        else
+            render 'edit'
+        end
+    end
+
+    def destroy
+        @article.destroy
+        redirect_to @article
+
+    end
+
+    private
+    
+    def set_article
+    @article  = Article.find(params[:id])
+    end
+
+    def article_params
+        params.require(:article).permit(:title,:description)
+    end
 end
